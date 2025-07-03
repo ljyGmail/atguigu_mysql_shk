8c52c5a7fbefaf14ab7d23ea2b35f35326298827
liangjinyong <liangjy881@gmail.com>
2025-07-03 09:56:12 +0900

    12 SQL概述与SQL分类

---
+++ 01_basics/03_basic_select.sql
@@ -0,0 +1,14 @@
+# 第03章 基本的SELECT语句
+
+# 1. SQL的分类
+/*
+DDL: 数据定义语言。 CREATE \ ALTER \ DROP \ RENAME \ TRUNCATE
+
+DML: 数据操作语言。 INSERT \ DELETE \ UPDATE \ SELECT (重中之重)
+
+DCL: 数据控制语言。 COMMIT \ ROLLBACK \ SAVEPOINT \ GRANT \ REVOKE
+
+学习技巧: 大处着眼、小处着手。
+ */
+
+
