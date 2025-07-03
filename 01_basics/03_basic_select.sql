d679c35dc3dd17af729ae8ea299c9f2803959aee
liangjinyong <liangjy881@gmail.com>
2025-07-03 22:54:28 +0900

    15 列的别名、去重、NULL、DESC等操作

--- 01_basics/03_basic_select.sql
+++ 01_basics/03_basic_select.sql
@@ -58,3 +58,55 @@ FROM employees;
 SELECT employee_id, last_name, salary
 FROM employees;
 
+# 6. 列的别名
+# AS 全称: alias(别名)，可以省略
+# 列的别名可以使用一对双引号("")引起来，不要使用单引号('')。
+SELECT employee_id emp_id, last_name AS lname, department_id "部门id", salary * 12 "annual sal"
+FROM employees;
+
+# 7. 去除重复行
+# 查询员工表中一共有哪些部门id?
+# 错误的: 没有去重的情况
+SELECT department_id
+FROM employees;
+# 正确的: 去重的情况
+SELECT DISTINCT department_id
+FROM employees;
+
+# 错误的:
+/*
+SELECT salary, DISTINCT department_id
+FROM employees;
+ */
+
+# 仅仅是没有报错，但是没有实际意义。
+SELECT DISTINCT salary, department_id
+FROM employees;
+
+# 8. 空值参与运算
+## 1. 空值: NULL
+## 2. NULL不等同于0，''，'NULL'
+SELECT *
+FROM employees;
+
+## 3. 空值参与运算: 结果一定也为空
+SELECT employee_id, salary "月工资", salary * (1 + commission_pct) * 12 "年工资", commission_pct
+FROM employees;
+
+# 实际问题的解决方案: 引入IFNULL
+SELECT employee_id, salary "月工资", salary * (1 + IFNULL(commission_pct, 0)) * 12 "年工资", commission_pct
+FROM employees;
+
+# 9. 着重号 ``
+SELECT *
+FROM `order`;
+
+# 10. 查询常数
+SELECT '尚硅谷', 123, employee_id, last_name
+FROM employees;
+
+# 11. 显示表结构
+DESCRIBE employees; # 显示了表中字段的详细信息
+
+DESC departments;
+
